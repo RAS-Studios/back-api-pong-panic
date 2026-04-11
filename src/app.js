8263643b63ba31e82325b281215bff12a0430a1e
Sonny Naidja <sonny.naidja@protonmail.com>
2026-04-11 17:18:24 +0200

    [+] develop: adding CORS for front testing and backend log

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const userRoute = require('./features/users/users.route');
 const matchRoute = require('./features/matches/matches.route')
+const cors = require('cors');
 
 require('dotenv').config();
 const path = require("path");
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173', // ton frontend en dev
+    credentials: true
+}));
 
 app.use('/api/users', userRoute);
 app.use('/api/matches', matchRoute);
